8cb30179ba277580b30525953344a98609b3aab1
Abusaleh2002 <mdabusalehmusa2021@gmail.com>
2023-10-25 18:59:18 +0600

    javascript_project jquery js wse_musa file uplode

---
+++ js/app.js
@@ -0,0 +1,39 @@
+$(function(){
+    $(window).scroll(function(){
+      let scroll = $(this).scrollTop()
+      if(scroll > 600 ){
+        $(".navbar-section").addClass("nav_bg_chang")
+      }else{
+        $(".navbar-section").removeClass("nav_bg_chang"),1000
+      }
+
+      if(scroll > 800){
+        $(".back_to_top").fadeIn()
+      }else{
+        $(".back_to_top").fadeOut()
+
+      }
+    })
+    $(".back_to_top").click(function(){
+        $("html, body").scrollTop(0)
+
+    })
+
+    
+})
+
+
+
+
+// Jquery........
+// Veno Box
+new VenoBox({
+  selector: '.my-image-links',
+  numeration: true,
+  infinigall: true,
+  share: true,
+  spinner: 'rotating-plane'
+});
+// Wow function for annimation dely
+new WOW().init();
+
